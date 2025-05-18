2d60c4f8656451765a9488fa7b49b2dd30a61b60
Corentin75 <corentindetournay05@gmail.com>
2025-05-18 19:24:58 +0200

    Project 1

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-  const backToTopBtn = document.getElementById("backToTopBtn");
+const backToTopBtn = document.getElementById("backToTopBtn");
 
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 10) {
-      backToTopBtn.classList.add("show");
-    } else {
-      backToTopBtn.classList.remove("show");
-    }
-  });
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    backToTopBtn.classList.add("show");
+  } else {
+    backToTopBtn.classList.remove("show");
+  }
+});
 
-  backToTopBtn.addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
