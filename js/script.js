8a6ddd2e60573de7618506cf8d74528028c65c0b
Corentin75 <corentindetournay05@gmail.com>
2025-05-25 16:08:14 +0200

    images overview

--- js/script.js
+++ js/script.js
@@ -11,3 +11,25 @@ window.addEventListener("scroll", () => {
 backToTopBtn.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+
+function openModal(src) {
+  const modal = document.getElementById('imageModal');
+  const modalImg = document.getElementById('modalImage');
+  modalImg.src = src;
+  modal.classList.remove('hidden');
+
+  document.addEventListener('keydown', handleEscClose);
+}
+
+function closeModal() {
+  const modal = document.getElementById('imageModal');
+  modal.classList.add('hidden');
+  document.removeEventListener('keydown', handleEscClose);
+}
+
+function handleEscClose(e) {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+}
